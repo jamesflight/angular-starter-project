61dcacc1d95352d61dfc46949c851114eef918e5
James Flight <jamesflight2006@gmail.com>
2014-10-27 09:33:10 +0000

    Fined tuned jslint

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var ngAnnotate = require('gulp-ng-annotate');
 var sourcemaps = require('gulp-sourcemaps');
 var ngHtml2Js = require("gulp-ng-html2js");
 var minifyHtml = require("gulp-minify-html");
+var jshint = require('gulp-jshint');
 
 gulp.task('buildJs', function(){
    return gulp.src([
@@ -12,12 +13,12 @@ gulp.task('buildJs', function(){
        'bower_components/angular-route/angular-route.js',
        'src/**/*.js'
    ])
-       .pipe(sourcemaps.init())
-       .pipe(ngAnnotate())
-       .pipe(concat('main.min.js'))
-       .pipe(uglify())
-       .pipe(sourcemaps.write('dist/'))
-       .pipe(gulp.dest('dist'));
+        .pipe(sourcemaps.init())
+        .pipe(ngAnnotate())
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('dist/'))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('buildHtml', function(){
@@ -31,8 +32,17 @@ gulp.task('buildHtml', function(){
       .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default',['buildJs','buildHtml']);
+gulp.task('lint', function(){
+    return gulp.src(['src/**/*.js'])
+    .pipe(jshint({
+            globalstrict:true,
+            "predef": ["angular"]
+        }))
+    .pipe(jshint.reporter('default'))
+});
+
+gulp.task('default',['lint', 'buildJs','buildHtml']);
 
 gulp.task('watch', function() {
-    gulp.watch('src/**/*.js', ['build']);
+    gulp.watch('src/**/*.*', ['default']);
 });
--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
-'use strict';
+"use strict";
 
-// Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
